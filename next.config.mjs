d26dbd0044775733ac89f7daf81a72dc87b9d15a
NirmaQureshi1234 <nirmaqureshi786@gmail.com>
2025-01-21 22:13:23 -0800

    more-fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,35 +1,15 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//         domains: ['cdn.sanity.io'], // Add Sanity's CDN domain
-//       },
-// };
-
-// export default nextConfig;
-
- 
- 
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images: {
-//       domains: ['cdn.sanity.io'],
-//     },
-//   };
-  
-//   export default nextConfig;
-  
-
-
-
-
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['cdn.sanity.io'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
